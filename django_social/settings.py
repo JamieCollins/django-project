20a6bf43e8b1dc8c8a1a8d09df483c4bac4dab5b
JamieCollins <Teyouneek@hotmail.com>
2023-05-12 18:25:08 +0000

    Deploy 3

--- django_social/settings.py
+++ django_social/settings.py
@@ -10,11 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
-import os
 import dj_database_url
-if os.path.isfile('env.py'):
+if os.path.isfile("env.py"):
     import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'social',
 ]
 
