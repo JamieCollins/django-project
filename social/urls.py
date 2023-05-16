7e71d83b5c01d9ae22d69675df04dcb6ff0d8e01
JamieCollins <Teyouneek@hotmail.com>
2023-05-16 18:16:38 +0000

    Update urls.py file for category model

--- social/urls.py
+++ social/urls.py
@@ -1,8 +1,10 @@
 from . import views
 from django.urls import path
+from .views import CategoryList
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+    path('categories/', CategoryList.as_view(), name='category_list'),
 ]
